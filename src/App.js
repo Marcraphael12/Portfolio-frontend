4d71608edd118e4cb24553947744f22b9a2532d1
Marc raphael <marcraphael979@gmail.com>
2024-01-09 00:22:46 +0100

    Reorganize components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navigation from './components/Navigation';
+import Navigation from './components/navigation/Navigation';
 import Welcome from './pages/Welcome';
 
 
