0e9487d70d02e33984fe824b14f1cfeeefc95bb8
Marc raphael <marcraphael979@gmail.com>
2024-01-13 02:54:29 +0100

    Change the text for logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,16 @@ import '../../styles/header.scss';
 const Header = () => {
   return (
     <header className="df-i">
-      <div className="logo">Marc raphael.</div>
+      <div className="logo">
+
+        {/* While waiting for a new log image */}
+        <span>
+          Simple
+        </span>
+        <span>
+          Code
+        </span>
+      </div>
       <div className="menu-btn df-c">
         <button type="button" className="df-c">
           <span></span><span></span><span></span>
