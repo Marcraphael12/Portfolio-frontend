add5ee290a6a94e34243c72166b44780132bff0e
Marc raphael <marcraphael979@gmail.com>
2024-01-16 19:00:49 +0100

    Create project navigation

---
+++ src/components/projects/navigation.jsx
@@ -0,0 +1,23 @@
+export default function Projects_nav () {
+	return (
+		<div className="project-nav">
+			<ul>
+				<li>
+					<a>
+						All
+					</a>
+				</li>
+				<li>
+					<a>
+						Group
+					</a>
+				</li>
+				<li>
+					<a>
+						Solo
+					</a>
+				</li>
+			</ul>
+		</div>
+	)
+}
