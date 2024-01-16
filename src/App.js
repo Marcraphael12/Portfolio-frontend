cdcfb07d439390b3012c08612c8ea4de8a2b499e
Marc raphael <marcraphael979@gmail.com>
2024-01-16 19:00:28 +0100

    Add projects page to the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
 import Home from './pages/Home';
+import Projects from './pages/Projects'
 
 
 const App = () => (
@@ -10,6 +11,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Projects />
       </>
     </BrowserRouter>
 );
