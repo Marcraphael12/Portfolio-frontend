54094f5d9a966e0030ad51653920c6a76136d61c
Marc raphael <marcraphael979@gmail.com>
2024-02-03 03:23:26 +0100

    pages/Home: Simply header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,31 @@
-import Name from "../components/home/Name";
-import Text from "../components/home/Text";
-import Buttons from "../components/home/Buttons";
-
 import '../styles/home.scss';
 const Home = () => {
   return (
     <section id="Home" className="df-c">
+
       <div className="df-c blur">
-        <Name />
-        <Text />
-        <Buttons />
+
+        <h1 className="name df-c">
+          <span>
+            Hello
+          </span>
+          <span>
+            I&lsquo;m Marc
+          </span>
+        </h1>
+
+        <div className="text">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sit voluptate quis aliquam, harum minus dignissimos facilis
+          </p>
+        </div>
+
+        <div className="buttons">
+          <button>
+            get in touch
+          </button>
+        </div>
+        
       </div>
     </section>
   )
