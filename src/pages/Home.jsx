df2167a75873cae814dc7e9a8a9c26631777da08
Marc raphael <marcraphael979@gmail.com>
2024-02-04 20:39:45 +0100

    Update content
    - remove button
    - add social medias
    - change texte

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,48 @@
 import '../styles/home.scss';
+
+import { faAngellist, faGithub, faLinkedin, faMedium, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Home = () => {
   return (
-    <section id="Home" className="df-c">
+    <section id="Home" className="df-c justify-end mb-10">
 
-      <div className="df-c blur">
+      <div className="df-c items-center justify-between p-6">
 
-        <h1 className="name df-c">
-          <span>
-            Hello
+        <div className="text df-c items-center">
+          <span className='text-[#0400ff] font-bold'>
+            Hello, I am
           </span>
-          <span>
-            I&lsquo;m Marc
-          </span>
-        </h1>
-
-        <div className="text">
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sit voluptate quis aliquam, harum minus dignissimos facilis
+          <h1 className=''>
+            Marc raphael
+          </h1>
+          <p className='text-[#0400ff] pl-3 pr-3 bg-white'>
+            Full-stack developper
           </p>
         </div>
-
-        <div className="buttons">
-          <button>
-            get in touch
-          </button>
-        </div>
         
+        <ul className='flex'>
+          <li className='w-40 text-lg flex flex-wrap justify-between text-white'>
+            <a href="" className='hover:text-black'>
+              <FontAwesomeIcon icon={faAngellist}/>
+            </a>
+
+            <a href="" className='hover:text-blue-700'>
+              <FontAwesomeIcon icon={faLinkedin}/>
+            </a>
+
+            <a href="" className='hover:text-[#0400ff45]'>
+              <FontAwesomeIcon icon={faGithub}/>
+            </a>
+
+            <a href="" className='hover:text-red-600'>
+              <FontAwesomeIcon icon={faYoutube}/>
+            </a>
+
+            <a href="" className='hover:text-black'>
+              <FontAwesomeIcon icon={faMedium}/>
+            </a>
+          </li>
+        </ul>
       </div>
     </section>
   )
