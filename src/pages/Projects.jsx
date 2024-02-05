1e7011cc475f1089120c7ab9230fc8095639e0c1
Marc raphael <marcraphael979@gmail.com>
2024-02-05 16:21:30 +0100

    Add hire me button

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,15 +1,22 @@
-import Projects_nav from "../components/projects/navigation";
+// import Projects_nav from "../components/projects/navigation";
 import Cards from "../components/projects/cards"
-import Buttons from "../components/home/Buttons";
-// import Card from "../components/projects/card";
+import Card from "../components/projects/card";
 import "../styles/projects.scss";
 const Projects = () => {
   return (
     <section id="Projects">
-      <Projects_nav />
+      {/* <Projects_nav /> */}
       <Cards />
-      {/* <Card /> */}
-      <Buttons />
+      <Card />
+      <div className='Hire-btn flex flex-wrap justify-center items-center p-3'>
+				<h3 className='text-lg font-bold text-white'>
+					Do you have a project for me?
+				</h3>
+
+				<button className='flex items-center text-md font-semibold'>
+					Let&apos;s do it!
+				</button>
+			</div>
     </section>
   );
 };
