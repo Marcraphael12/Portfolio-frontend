d709d67d1499df9d116c4cd6a183690d023f62f9
Marc raphael <marcraphael979@gmail.com>
2024-02-08 01:16:29 +0100

    Only import the construction page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import Header from './components/header/Header';
-import Home from './pages/Home';
-import Projects from './pages/Projects'
-import Contact from './pages/Contact';
+// import Home from './pages/Home';
+// import Projects from './pages/Projects'
+// import Contact from './pages/Contact';
+// import About from './pages/About';
 import Footer from './pages/Footer';
-import About from './pages/About';
+
+import Construction from './pages/construction';
 
 
 const App = () => (
       <>
         <Header />
-        <Home />
+        {/* <Home />
         <About />
         <Projects />
-        <Contact />
+        <Contact /> */}
+        <Construction />
         <Footer />
       </>
 );
