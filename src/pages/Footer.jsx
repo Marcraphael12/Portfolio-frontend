0a1f5744fab0d3778c2fd3849fa76fc005f68980
Marc raphael <marcraphael979@gmail.com>
2024-02-08 01:34:16 +0100

    Add social links

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -6,19 +6,19 @@ const Footer = ()=> {
 		<footer>
 			<ul className='flex flex-wrap justify-between items-center content-center justify-items-center'>
 				<li className='w-40 text-lg max-w-40 flex justify-between'>
-					<a href="">
+					<a href="https://angel.co/u/marc-raphael12">
 						<FontAwesomeIcon icon={faAngellist}/>
 					</a>
 
-					<a href="" className='text-white'>
+					<a href="https://www.linkedin.com/in/marc-raphael-essogo-bidzogo/" className='text-white'>
 						<FontAwesomeIcon icon={faLinkedin}/>
 					</a>
 
-					<a href="" className='text-white'>
+					<a href="https://github.com/Marcraphael12/" className='text-white'>
 						<FontAwesomeIcon icon={faGithub}/>
 					</a>
 
-					<a href="" className='text-white'>
+					<a href="https://www.youtube.com/@simplecode5481/videos" className='text-white'>
 						<FontAwesomeIcon icon={faYoutube}/>
 					</a>
 
