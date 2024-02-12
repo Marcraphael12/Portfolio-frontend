b93abcc19bc9432a71843a8c847cc668e14e5245
Marc raphael <marcraphael979@gmail.com>
2024-02-12 17:54:39 +0100

    Update layout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,16 @@
 import '../styles/about.scss'
 const About = ()=> {
 	return (
-		<section id="About" className='flex flex-col'>
-			<div className='p-3 text'>
+		<>
+		<section id="About" className='flex flex-col justify-between'>
+			<div className='text'>
 				<h1 className='text-3xl font-bold mb-6'>Coding is a passion!</h1>
 				<p className='mb-6'>
 					Hello, I am Marc; A 24 -year-old full-stack developer. I started programming in 2019 and continued to specialize in 2021. These days, along with programming, I study about startups and businesses, and besides all this, I play the dulcimer and watch my favorite series. am working!
 				</p>
 			</div>
 
-			<div className='bg-[#0400ff18] p-6 skills'>
+			<div className='bg-[#0400ff18] skills flex flex-col mt-4 border border-black'>
 				<h3 className='text-xl font-bold'>
 					Tech Skills
 				</h3>
@@ -36,7 +37,7 @@ const About = ()=> {
 				</ul>
 			</div>
 
-			<div className='flex flex-col bg-[#0400ff18] p-6 skills'>
+			<div className='flex flex-col bg-[#0400ff18] skills mt-4 border border-black'>
 				<h3 className='text-xl font-bold'>
 					Languages skills
 				</h3>
@@ -58,17 +59,18 @@ const About = ()=> {
 				</div>
 			</div>
 
-			<div className='Hire-btn flex flex-wrap justify-center items-center p-3'>
-				<h3 className='text-lg font-bold text-white'>
-					I am available for remote jobs!
-				</h3>
-
-				<button className='flex items-center text-md font-semibold'>
-					Hire me now!
-				</button>
-			</div>
 
 		</section>
+		<div className='Hire-btn flex flex-wrap justify-center items-center p-3'>
+			<h3 className='text-lg font-bold text-white'>
+				I am available for remote jobs!
+			</h3>
+
+			<button className='flex items-center text-md font-semibold'>
+				Hire me now!
+			</button>
+		</div>
+		</>
 	)
 }
 
